bba090a85ee5d7f842950eb1e6710c0fc5164324
K011EGA <106887556+K011EGA@users.noreply.github.com>
2022-06-23 19:09:47 +0300

    Add files via upload

---
+++ lab5/Palindrom.cpp
@@ -0,0 +1,100 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	HANDLE writePipe = (HANDLE)atoi(argv[1]);
+	HANDLE readPipe = (HANDLE)atoi(argv[2]);
+	HANDLE R = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"R");
+	HANDLE SR = OpenEvent(EVENT_ALL_ACCESS, TRUE, L"SR");
+	cout << "В ожидание получения размерности массива" << endl;
+	WaitForSingleObject(R, INFINITE);
+	int n;
+	DWORD dwBytesRead;
+	if (!ReadFile(readPipe, &n, sizeof(n), &dwBytesRead, NULL))
+	{
+		cout << "Read from the pipe failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return 0;
+	}
+	char* s = new char[n];
+	DWORD dwBytesRead2;
+	for (int i = 0; i < n; i++)
+	{
+		if (!ReadFile(readPipe, &s[i], sizeof(s), &dwBytesRead2, NULL))
+		{
+			cout << "Read from the pipe failed.\n";
+			cout << "Press any key to finish.\n";
+			_getch();
+			return 0;
+		}
+	}
+
+	system("cls");
+	cout << "Simple получил размер массива: " << n << endl;
+
+	char x;
+	cout << "Введите разделитель" << endl;
+	cin >> x;
+	cout << "Полученный разделитель " << x << endl;
+
+	int k = 0;
+	bool key  = true;
+	SetEvent(SR);
+	for (int i = 0; i < n; i++)
+	{
+		//cout << s[i] << " ";
+		if (s[i] == x)
+		{
+			for (int j = 0; j < (i - k) / 2; j++)
+			{
+				if (s[k + j] != s[i - j - 1])
+				{
+					key = false;
+					break;
+				}
+			}
+			//cout << key;
+			if (key)
+			{
+				for (int j = k; j < i; j++)
+				{
+					cout << s[j];
+					DWORD dwBytesWritten;
+					if (!WriteFile(writePipe, &s[j], sizeof(s), &dwBytesWritten, NULL))
+					{
+						cout << "Write to file failed.\n";
+						cout << "Press any key to finish.\n";
+						_getch();
+						return GetLastError();
+					}
+					SetEvent(SR);
+					WaitForSingleObject(R, INFINITE);
+				}
+				DWORD dwBytesWritten;
+				if (!WriteFile(writePipe, &s[i], sizeof(s), &dwBytesWritten, NULL))
+				{
+					cout << "Write to file failed.\n";
+					cout << "Press any key to finish.\n";
+					_getch();
+					return GetLastError();
+				}
+				SetEvent(SR);
+				WaitForSingleObject(R, INFINITE);
+
+			}
+			k = i + 1;
+			key = true;
+		}
+
+	}
+	CloseHandle(writePipe);
+	CloseHandle(readPipe);
+	cout << "\nВсе найденные полиндромы были отправлены\n";
+	cout << "Press any key to exit.\n";
+	_getch();
+	return 0;
+}
---
+++ lab5/Server.cpp
@@ -0,0 +1,94 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	TCHAR lpszComLine[80];
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	HANDLE hWritePipe, hReadPipe;
+	HANDLE R = CreateEvent(NULL, FALSE, FALSE, L"R");
+	HANDLE SR = CreateEvent(NULL, FALSE, FALSE, L"SR");
+	SECURITY_ATTRIBUTES sa;
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	sa.lpSecurityDescriptor = NULL;
+	sa.bInheritHandle = TRUE;
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, 0))
+	{
+		cout << "Create pipe failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return GetLastError();
+	}
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	wsprintf(lpszComLine, L"Palindrom.exe %d %d", (int)hWritePipe, (int)hReadPipe);
+	if (!CreateProcess(NULL, lpszComLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+	{
+		cout << "Create process failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return GetLastError();
+	}
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	int n;
+	cout << "Введите размер массива" << endl;
+	cin >> n;
+	char* s = new char[n];
+	cout << "Введите массив" << endl;
+	cin >> s;
+	DWORD dwBytesWritten;
+	if (!WriteFile(hWritePipe, &n, sizeof(n), &dwBytesWritten, NULL))
+	{
+		cout << "Write to file failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return GetLastError();
+	}
+	for(int i = 0; i< n;i++)
+	{
+		if (!WriteFile(hWritePipe, &s[i], sizeof(s), &dwBytesWritten, NULL))
+		{
+			cout << "Write to file failed.\n";
+			cout << "Press any key to finish.\n";
+			_getch();
+			return GetLastError();
+		}
+	}
+
+	int i = 0;
+	cout << "Server отправил : " << n << " " << s <<endl;
+	SetEvent(R);
+	int k = 0;
+	WaitForSingleObject(SR, INFINITE);
+	while (WaitForSingleObject(SR, 1000) == false) {
+		k++;
+		char* nData = new char[n];
+		DWORD dwBytesRead;
+		if (!ReadFile(hReadPipe, &nData[i], sizeof(nData), &dwBytesRead, NULL))
+		{
+			cout << "Read from the pipe failed.\n";
+			cout << "Press any key to finish.\n";
+			_getch();
+			return GetLastError();
+		}
+		cout << "" << nData[i];
+		i++;
+		SetEvent(R);
+	}
+	cout << endl;
+	if (k == 0) {
+		cout << "Ничего не было получено" << endl;
+	}
+	else {
+		cout << "Все лексемы были получены" << endl;
+	}
+	CloseHandle(hWritePipe);
+	CloseHandle(hReadPipe);
+	cout << "Press any key to exit.\n";
+	_getch();
+	return 0;
+}
