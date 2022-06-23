dc1a25a2ae178fe57ad756ac0eced983492d0e5e
K011EGA <106887556+K011EGA@users.noreply.github.com>
2022-06-23 19:07:05 +0300

    Add files via upload

---
+++ lab2/Child.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <unordered_map>
+#include <windows.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    int size = argc-1;
+    char* mas = new char[size];
+    for (int i = 0; i < size; i++) {
+        mas[i] = *argv[i + 1];
+    }
+
+    unordered_map<char, int> hmap;
+
+    for (int i = 0; i < size; i++)
+        hmap[mas[i]]++;
+
+    string oddString = "";
+    for (auto x : hmap)
+    {
+        if (x.second % 2 != 0)
+            oddString = oddString + x.first;
+    }
+
+
+    string firstHalf = "", secondHalf = "";
+
+    for (auto x : hmap)
+    {
+
+        string s(x.second / 2, x.first);
+
+        firstHalf = firstHalf + s;
+        secondHalf = s + secondHalf;
+    }
+
+    if (oddString.length() != 0)
+    {
+        firstHalf = firstHalf + oddString[0];
+    }
+
+    secondHalf = secondHalf + "|";
+    for (int i = 1; i < oddString.length(); i++)
+        secondHalf = secondHalf + oddString[i];
+
+
+    cout << (firstHalf + secondHalf);
+    cin.get();
+}
---
+++ lab2/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <Windows.h>
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include <string>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RUS");
+	string line = "Child.exe ";
+
+	cout << "Введите кол-во элементов и массив: " << endl;
+	int x;
+	cin >> x;
+	char* mas = new char[x];
+
+	cin.ignore(255,' ');
+	cin.getline(mas,x+1);
+	
+	for (int i = 0; i < x; i++)
+		line = line + " " + mas[i];
+
+	wstring c(line.begin(), line.end());
+	LPWSTR command_line = &c[0];
+	STARTUPINFO si;
+	PROCESS_INFORMATION piApp;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	LPWSTR title = (LPWSTR)L"NAZVANIE";
+	si.lpTitle = title;
+
+	boolean check = CreateProcess(NULL, command_line, NULL, NULL, FALSE,
+		CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp);
+
+	if (!check) {
+		_cputs("process not created");
+		return 0;
+	}
+	_cputs("The new process is created.\n");
+	WaitForSingleObject(piApp.hProcess, INFINITE);
+	CloseHandle(piApp.hThread);
+	CloseHandle(piApp.hProcess);
+
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	line = "Count.exe";
+	wstring str(line.begin(), line.end());
+	command_line = &str[0];
+	boolean check2 = CreateProcess(NULL, command_line, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp);
+	if (check2) 
+	{
+		cout << "New process has created";
+	}
+	SetPriorityClass(piApp.hProcess, HIGH_PRIORITY_CLASS);
+	Sleep(4000);
+	TerminateProcess(piApp.hProcess, 0);
+	CloseHandle(piApp.hThread);
+	CloseHandle(piApp.hProcess);
+	ExitProcess(0);
+}
